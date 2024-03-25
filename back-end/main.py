3d33f0c82a855fbc300f20abc27a7430f0a6a791
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-03-25 19:51:46 -0300

    Integração Fron-End / Back-End: Vite-React-Tailwind CSS / Python-Flask

---
+++ back-end/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+@app.route("/api/users", methods=['GET'])
+def users():
+    return jsonify(
+        {
+            "users": [
+                'samuka',
+                'zach',
+                'jessie'
+            ]
+        }
+    )
+    
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 export function App() {
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api/users");
+    setArray(response.data.users);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
   return (
     <h1 className="text-cinza-light font-bold underline">
-      Hello world!
+      Hello World
+      <div className="content">
+        <p>
+          {
+            array.map((user, index) => (
+              <div key={index}>
+                <span>{user}</span>
+                <br></br>
+              </div>
+            ))
+          }
+        </p>
+      </div>
     </h1>
   )
 }
