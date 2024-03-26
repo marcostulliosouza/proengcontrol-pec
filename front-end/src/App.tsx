654d90820c32158fe1bccd7e4d82c59a949c7d8d
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-03-26 12:07:22 -0300

    FEAT: implementação do front-end da Login_Page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,33 +1,55 @@
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import logo from './assets/icon_pec.svg'
+import user from './assets/user.png'
+import key from './assets/key.png'
 
 export function App() {
-  const [array, setArray] = useState([]);
-
-  const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8080/api/users");
-    setArray(response.data.users);
-  };
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-
   return (
-    <h1 className="text-cinza-light font-bold underline">
-      Hello World
-      <div className="content">
-        <p>
-          {
-            array.map((user, index) => (
-              <div key={index}>
-                <span>{user}</span>
-                <br></br>
-              </div>
-            ))
-          }
-        </p>
+    <div className="flex flex-col bg-cinza max-w-[50vh] w-full h-[70vh] fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md ">
+      <div className="flex flex-col items-center fixed overflow-hidden left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 text-pec font-semibold">
+        <div className="flex items-center ">
+          <img src={logo} alt="PEC" />
+          <div className="flex flex-col p-2">
+            <span className='text-xl'>PEC</span>
+          </div>
+        </div>
+        <span className='text-sm'>ProEngControl</span>
+        <span className='text-cinza-medium_dark'>Bem-Vindo!</span>
       </div>
-    </h1>
+      <div className='grid gap-2 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-left font-normal tracking-tight rounded-md'>
+        <form className='flex items-center border-b'>
+          <img className='w-5 pr-1 opacity-50' src={user} alt="user" />
+          <input className='bg-cinza indent-1'
+            type="text"
+            placeholder='Login'
+          />
+        </form>
+        <form className='flex items-center border-b'>
+          <img className='w-5 pr-1 opacity-50' src={key} alt="key" />
+          <input className='bg-cinza indent-1'
+            type="text"
+            placeholder='Senha'
+          />
+        </form>
+        <div className='flex items-center gap-2 font-normal'>
+          <button
+            id='rememberUser'
+            type='button'
+            className='box-border h-5 w-5 bg-cinza-extra_dark focus:bg-pec'
+          >
+          </button>
+          <span>Lembrar usuário</span>
+        </div>
+
+        <script>
+          const rememberUser =
+        </script>
+      </div>
+      <button
+        type="submit"
+        className='fixed left-1/2 top-3/4 -translate-x-1/2 -translate-y-1/2 font-bold border border-pec rounded-md box-border h-10 w-32 bg-pec text-cinza'
+      >
+        Entrar
+      </button>
+    </div>
   )
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -10,7 +10,9 @@ export default {
       'cinza': {
         light: '#f1f1f1',
         DEFAULT: '#e3e3e3',
+        medium_dark: '#444444',
         dark: '#d9d9d9',
+        extra_dark: '#b8b8b8',
       } 
       
     }
