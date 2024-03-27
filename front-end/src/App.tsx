e43a2e0b08b509c0fd498f9d19506530763e86c7
Samuel Grontoski <contatosamuelgrontoski@gmail.com>
2024-03-26 23:37:43 -0300

    FEAT: react router dom

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,65 +1,16 @@
-import logo from './assets/icon_pec.svg'
-import user from './assets/user.png'
-import key from './assets/key.png'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './pages/page_login';
+import { Chamados } from './pages/page_chamados';
 
 export function App() {
 
   return (
 
-    <div className='grid grid-cols-2 h-screen bg-pec'>
-      <div className='bg-cinza max-w-[50vh] w-full h-[70vh] fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md'>
-        <div>
-          <main className="flex flex-col mt-20 gap-10">
-            <header className="flex flex-col items-center text-pec font-semibold">
-              <div className="flex items-center gap-2">
-                <img src={logo} alt="PEC" />
-                <h1 className='text-xl'>PEC</h1>
-              </div>
-              <div className="flex flex-col items-center gap-2">
-                <p className='text-sm'>ProEngControl</p>
-                <p className='text-cinza-medium_dark'>Bem-Vindo!</p>
-              </div>
-            </header>
-            <form
-              className="flex flex-col items-center gap-4 font-normal"
-            >
-              <div className="flex items-center gap-2">
-                <img className='w-5 pr-1 opacity-50' src={user} alt="user" />
-                <input className='bg-cinza indent-1'
-                  type="text"
-                  placeholder='Login'
-                />
-              </div>
-              <div className="flex items-center gap-2">
-                <img className='w-5 pr-1 opacity-50' src={key} alt="key" />
-                <input className='bg-cinza indent-1'
-                  type="text"
-                  placeholder='Senha'
-                />
-              </div>
-              <div className='flex items-center gap-2 font-normal -translate-x-1/4'>
-                <button
-                  id='rememberUser'
-                  type='button'
-                  className='box-border h-5 w-5 bg-cinza-extra_dark rounded-md focus:bg-pec'
-                >
-                </button>
-                <span className='text-pec'>
-                  Lembrar usuário
-                </span>
-              </div>
-              <footer className="flex flex-col gap-8 pt-10">
-                <button
-                  type="submit"
-                  className='fixed left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold border border-pec rounded-md box-border h-10 w-32 bg-pec text-cinza'
-                >
-                  Entrar
-                </button>
-              </footer>
-            </form>
-          </main>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/chamados" element={<Chamados/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ front-end/src/pages/page_chamados.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export function Chamados () {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <Link to="/">retornar a página inicial</Link>
+        </div>
+    )
+}
