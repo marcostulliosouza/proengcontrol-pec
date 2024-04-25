96e0336f9ed561dad64a8eb05c2d76c8bdb02477
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-04-25 12:15:57 -0300

    REFACTOR: botões do menu

--- front-end/src/components/button_menu.tsx
+++ front-end/src/components/button_menu.tsx
@@ -5,7 +5,7 @@ export function MenuButton({ name, link, icon: Icon }) {
         <Link to={link}>
             <button 
                 type="submit" 
-                className="grid justify-items-center items-center w-[100px] h-[100px] bg-cinza-300 rounded text-sm text-cinza-500 font-semibold shadow-md hover:text-black hover:shadow-lg">
+                className="grid justify-items-center items-center w-[100px] h-[100px] bg-cinza-300 rounded text-sm text-pec font-semibold shadow-md hover:shadow-lg hover:scale-110 transition duration-200">
                     <Icon className="w-7 h-7 mt-2" />
                     <p>{name}</p>
             </button>
