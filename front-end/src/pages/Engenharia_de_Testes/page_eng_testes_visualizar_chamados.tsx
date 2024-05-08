52d87fd254e96b81373d82c3ab102ef9152620c0
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-05-08 13:13:48 -0300

    att

--- front-end/src/pages/Engenharia_de_Testes/page_eng_testes_visualizar_chamados.tsx
+++ front-end/src/pages/Engenharia_de_Testes/page_eng_testes_visualizar_chamados.tsx
@@ -180,7 +180,14 @@ export function VisualizarChamados() {
                     <div className='text-4xl py-4 text-pec font-bold p-10 mt-10'>
                         CHAMADOS EM ATENDIMENTO
                     </div>
-                    <Carousel showArrows={false} showStatus={false} infiniteLoop={true} autoPlay={autoPlay} autoFocus interval={5000}>
+                    <Carousel 
+                        showArrows={false}
+                        showStatus={false} 
+                        infiniteLoop={true} 
+                        autoPlay={autoPlay} 
+                        autoFocus 
+                        interval={5000}
+                    >
                         {dados
                             .filter((row: any) => row.cha_status === 2)
                             .map((row: any) => (
