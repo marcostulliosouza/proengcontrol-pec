aa13a3a3a5e0e169b256850472b3b59e4e90eedc
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-06-12 12:39:55 -0300

    REFACTOR: página de login

--- front-end/src/pages/page_login.tsx
+++ front-end/src/pages/page_login.tsx
@@ -1,10 +1,8 @@
-import logo from '../assets/icon_pec.svg'
-import { AiOutlineUser } from "react-icons/ai";
+import logo from '../assets/icon_pec.svg';
+import { AiOutlineUser, AiOutlineLoading3Quarters } from "react-icons/ai";
 import { GoKey, GoCheck } from "react-icons/go";
-import { useNavigate } from 'react-router-dom'
-import { useState, useEffect } from 'react';
-import { FormEvent } from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, FormEvent } from 'react';
 
 export function Login() {
   // Função para limpar o localStorage, isso garante que o navegador não armazene o token de autenticação quando o usuário saí da aplicação ou da logout
@@ -14,23 +12,25 @@ export function Login() {
       sessionStorage.clear();
     }
   };
+
   // UseEffect para limpar o localStorage quando o componente de login for chamado
   useEffect(() => {
     clearLocalStorage();
   }, []);
 
   const [isChecked, setIsChecked] = useState(false);
+  const [col_login, setUsername] = useState('');
+  const [col_senha, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleOnChange = () => {
     setIsChecked(!isChecked);
   };
 
-  const [col_login, setUsername] = useState('');
-  const [col_senha, setPassword] = useState('');
-  const navigate = useNavigate();
-
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch('http://172.17.4.23:5000/login', {
         method: 'POST',
@@ -49,14 +49,16 @@ export function Login() {
         } else {
           localStorage.setItem("saveUser", "false");
         }
-        navigate('/menu');
-      }
-      else {
+        setTimeout(() => {
+          navigate('/menu');
+        }, 2000);
+      } else {
         window.alert('Desculpe, parece que você digitou o login ou a senha incorretos. \nPor favor, verifique e tente novamente.');
+        setLoading(false);
       }
-    }
-    catch (error) {
+    } catch (error) {
       console.error('Erro ao fazer login:', error);
+      setLoading(false);
     }
   };
 
@@ -115,17 +117,25 @@ export function Login() {
               </label>
             </div>
             {/* Botão de entrar */}
-            <footer className="flex flex-col gap-8 pt-10">
+            <footer className="w-full flex justify-center">
               <button
                 type="submit"
-                className='fixed left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold rounded-md box-border h-10 w-32 bg-pec text-cinza-200'
+                className='font-bold rounded-md h-10 w-32 bg-pec text-cinza-200'
+                disabled={loading}
               >
-                Entrar
-              </button>
-            </footer>
-          </form>
-        </main>
-      </div>
+                {loading ? (
+                  <div className="flex items-center gap-2 p-2">
+                    Carregando
+                    <AiOutlineLoading3Quarters className="animate-spin" />
+                  </div>
+                ) : (
+                  'Entrar'
+                )}
+            </button>
+          </footer>
+        </form>
+      </main>
     </div>
-  )
+    </div >
+  );
 }
