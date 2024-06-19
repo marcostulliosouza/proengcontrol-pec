5519d7c4d546f8b0abe8a04c85695676acfc6d52
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-06-19 11:07:42 -0300

    REFACTOR

--- front-end/src/pages/Engenharia_de_Testes/page_eng_testes_abrir_chamado.tsx
+++ front-end/src/pages/Engenharia_de_Testes/page_eng_testes_abrir_chamado.tsx
@@ -210,7 +210,7 @@ export function AbrirChamado() {
                                 placeholder={chamado.cha_tipo === 1 ? '000000' : 'XXXXXX'}
                                 disabled={chamado.cha_tipo === 1}
                                 onKeyDown={handleKeyDown}
-                                className='indent-1 border border-1 border-black rounded p-2 shadow-sm bg-white h-10 w-full'
+                                className='indent-1 border border-1 border-black rounded p-2 shadow-sm bg-white h-10 w-full text-sm'
                             />
                             <p className='text-red-600 text-lg font-bold'>*</p>
                         </div>
